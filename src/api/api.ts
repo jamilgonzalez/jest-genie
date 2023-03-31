2a9575f30cd0f7b95eb02d04e68a6e0a3ad16ed4
jamilgonzalez <jamil.gonzalez@icloud.com>
2023-03-31 13:52:30 -0400

    dotenv config

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,23 +1,23 @@
 // move to separate file
 const openai = require('openai-api')
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'
 import * as config from './config.json'
 
-const parsedKey = dotenv.config({path: '/Users/jamilgonzalez/fixtures-generator-poc/.env'})
+const parsedKey = dotenv.config()
 
 // key generated on Mar 30th
 const api_key = parsedKey.parsed?.GPT_API_KEY
 const openai_client = new openai(api_key)
 
 export const gptRequest = async (prompt: string) => {
-    // todo: add error handling, add logging, add tests, add CI/CD
-    const response = await openai_client.complete({
-      engine: config.model_engine,
-      prompt: prompt,
-      max_tokens: config.max_tokens,
-      n: config.completions,
-      stop: config.stop,
-    })
-    
-    return response.data.choices[0].text.trim()
-  }
+  // todo: add error handling, add logging, add tests, add CI/CD
+  const response = await openai_client.complete({
+    engine: config.model_engine,
+    prompt: prompt,
+    max_tokens: config.max_tokens,
+    n: config.completions,
+    stop: config.stop,
+  })
+
+  return response.data.choices[0].text.trim()
+}
