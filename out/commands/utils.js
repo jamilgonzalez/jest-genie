53a4b9bf26a63a9a32248905f70e3ce8f56b84c0
Jamil Gonzalez <jamil.gonzalez@icloud.com>
2023-04-03 21:57:09 -0400

    Delete out directory

--- out/commands/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Command = void 0;
-var Command;
-(function (Command) {
-    Command["GenerateFixtures"] = "fixtures-generator-poc.generateFixtures";
-})(Command = exports.Command || (exports.Command = {}));
-//# sourceMappingURL=utils.js.map
