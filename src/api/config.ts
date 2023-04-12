4e5a1a30aef813b74db80df9d58e455f19791825
jamilgonzalez <jamil.gonzalez@icloud.com>
2023-04-12 11:00:24 -0400

    Up the token amount to half GPT-3.5 turbos total capacity

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,7 +3,7 @@ import { GPTModelConfig } from './types'
 export const config: GPTModelConfig = {
   model: 'gpt-3.5-turbo',
   completions: 1,
-  max_tokens: 1024,
+  max_tokens: 2043,
   stop: '\\n',
   temperature: 0.25,
   top_p: 0.9,
